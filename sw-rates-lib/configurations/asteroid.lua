70af36902bd1b1a5ed2ae22c784ec2323b40a35b
Simon Weinberger <mail@sweini.de>
2025-06-20 07:53:10 +0200

    use new dying_trigger_effect prototype read

--- sw-rates-lib/configurations/asteroid.lua
+++ sw-rates-lib/configurations/asteroid.lua
@@ -4,17 +4,15 @@ local logic = { type = "asteroid" } ---@type Rates.Configuration.Type
 
 logic.fill_progression = function(result, options)
     for _, entity in pairs(prototypes.get_entity_filtered { { filter = "type", type = "asteroid" } }) do
-        local generated ---@type Rates.Progression.SingleItem?
-        if (string.match(entity.name, "^huge%-.+%-asteroid$")) then
-            generated = progression.create.map_entity("big" .. string.sub(entity.name, 5), "*")
-        elseif (string.match(entity.name, "^big%-.+%-asteroid$")) then
-            generated = progression.create.map_entity("medium" .. string.sub(entity.name, 4), "*")
-        elseif (string.match(entity.name, "^medium%-.+%-asteroid$")) then
-            generated = progression.create.map_entity("small" .. string.sub(entity.name, 7), "*")
-        elseif (string.match(entity.name, "^small%-.+%-asteroid$")) then
-            generated = progression.create.space_asteroid(string.sub(entity.name, 7) .. "-chunk", "*")
+        local generated = {} ---@type Rates.Progression.MultiItemPost
+        for _, effect in ipairs(entity.dying_trigger_effect or {}) do
+            if (effect.type == "create-asteroid-chunk") then
+                generated[#generated + 1] = progression.create.space_asteroid(effect.asteroid_name, "*") ---@diagnostic disable-line:undefined-field
+            elseif (effect.type == "create-entity") then
+                generated[#generated + 1] = progression.create.map_entity(effect.entity_name, "*") ---@diagnostic disable-line:undefined-field
+            end
         end
-        if (generated) then
+        if (#generated > 0) then
             local id = "entity-with-health/dying/" .. entity.name .. "/*"
             result[id] = {
                 pre = {
--- sw-rates-lib/data-final-fixes.lua
+++ sw-rates-lib/data-final-fixes.lua
@@ -1,71 +1,12 @@
 local bigpack = require("__big-data-string2__.pack")
 
----@class (exact) Rates.ExtraData.DyingEffect
----@field type "entity" | "asteroid-chunk"
----@field name string
----@field repeat_count integer
----@field probability number
----@field count? integer
-
 ---@class (exact) Rates.ExtraData
----@field asteroid_dying table<data.EntityID, Rates.ExtraData.DyingEffect[]>
 ---@field fusion_reactor table<data.EntityID, { max_fluid_usage: data.FluidAmount, neighbour_connectable: data.NeighbourConnectable? }>
 
 ---@type Rates.ExtraData
 local store = {
-    asteroid_dying = {},
     fusion_reactor = {},
 }
-
----@param effect data.TriggerEffect?
----@param result Rates.ExtraData.DyingEffect[]
-local function get_dying_spawns(effect, result)
-    if (not effect) then
-        return
-    end
-
-    if (not effect.type) then
-        for _, eff in ipairs(effect) do
-            get_dying_spawns(eff, result)
-        end
-
-        return
-    end
-
-    if (effect.type == "create-asteroid-chunk") then
-        local repeat_count = effect.repeat_count or 1
-        local probability = effect.probability or 1
-        local count = effect.offsets and #effect.offsets
-        result[#result + 1] = {
-            type = "asteroid-chunk",
-            name = effect.asteroid_name,
-            repeat_count = repeat_count,
-            probability = probability,
-            count = count
-        }
-    elseif (effect.type == "create-entity") then
-        if (not data.raw["asteroid"][effect.entity_name]) then
-            return
-        end
-
-        local repeat_count = effect.repeat_count or 1
-        local probability = effect.probability or 1
-        local count = effect.offsets and #effect.offsets
-        result[#result + 1] = {
-            type = "entity",
-            name = effect.entity_name,
-            repeat_count = repeat_count,
-            probability = probability,
-            count = count
-        }
-    end
-end
-
-for _, asteroid in pairs(data.raw["asteroid"] or {}) do
-    local effects = {} ---@types Rates.ExtraData.DyingEffect[]
-    get_dying_spawns(asteroid.dying_trigger_effect, effects)
-    store.asteroid_dying[asteroid.name] = effects
-end
 
 for _, entity in pairs(data.raw["fusion-reactor"] or {}) do
     store.fusion_reactor[entity.name] = {
