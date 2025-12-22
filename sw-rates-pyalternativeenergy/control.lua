f91bd79c9778dd8614f32b5f3065be76693f0f36
Simon Weinberger <mail@sweini.de>
2025-12-22 06:59:31 +0100

    show warning for interfering AM:FM beacons

--- sw-rates-pyalternativeenergy/control.lua
+++ sw-rates-pyalternativeenergy/control.lua
@@ -2,6 +2,7 @@ local handler = require("__core__.lualib.event_handler")
 local configuration = require("__sw-rates-lib__.scripts.configuration")
 
 local configurations = {
+    require("configurations.py-beacon"),
     require("configurations.py-biofluid"),
     require("configurations.py-bitumenseep"),
     require("configurations.py-digsite"),
