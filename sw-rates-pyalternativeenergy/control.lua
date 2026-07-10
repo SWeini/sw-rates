284e524de73630bb9c1984056d711ef8dd9cfa3b
Simon Weinberger <mail@sweini.de>
2026-07-10 21:33:46 +0200

    upgrade pyae to 2.1

--- sw-rates-pyalternativeenergy/control.lua
+++ sw-rates-pyalternativeenergy/control.lua
@@ -10,18 +10,11 @@ local configurations = {
     require("configurations.py-power"),
     require("configurations.py-smartfarm"),
     require("configurations.py-solar"),
+    require("configurations.py-turd"),
 }
-
-local configuration_turd = require("configurations.py-turd")
 
 handler.add_libraries({ {
     add_remote_interface = function()
         configuration.register(configurations)
-
-        if (helpers.compare_versions(script.active_mods["pyalienlife"], "3.0.61") >= 0) then
-            configuration.register({
-                configuration_turd
-            })
-        end
     end
 } })
